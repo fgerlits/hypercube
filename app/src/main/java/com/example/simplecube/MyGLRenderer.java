2545f37fd4009f0239cdd8be58825dab0f03a828
Ferenc Gerlits <hypercube.viewer.app@gmail.com>
2019-08-28 15:13:06 +0200

    Enable depth checking

--- app/src/main/java/com/example/simplecube/MyGLRenderer.java
+++ app/src/main/java/com/example/simplecube/MyGLRenderer.java
@@ -40,6 +40,9 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // Set the background frame color
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
+        GLES20.glEnable(GLES20.GL_DEPTH_TEST);
+        GLES20.glDepthFunc(GLES20.GL_LESS);
+
         cube = new Cube();
     }
 
@@ -56,7 +59,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
         // Set the camera position (View matrix)
         float[] viewMatrix = new float[16];
